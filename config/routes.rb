9c5e94ed431fb8cd2a0c0fd06723f6572ba59281
Ryan <rwickman@memphis.edu>
2020-01-29 13:35:49 -0600

    Add welcome and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome', to: 'static_pages#welcome', as: 'welcome'
+  get 'about', to: 'static_pages#about', as: 'about'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
