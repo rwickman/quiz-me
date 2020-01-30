7819c2c4bd01adca683555a4f39f869118328f0b
Ryan <rwickman@memphis.edu>
2020-01-29 21:51:35 -0600

    Add form with passing data from controller to view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def welcome
+    features = [
+      "Choose from premade quizzes on a variety of topics",
+      "Make your own quizzes to customize your learning",
+      "Compare your scores with other users"
+    ]
     respond_to do |format|
-      format.html { render :welcome }
+      format.html { render :welcome, locals: { features: features } }
     end
   end
 
@@ -10,4 +15,31 @@ class StaticPagesController < ApplicationController
      format.html { render :about }
    end
  end
+
+ def contact
+  respond_to do |format|
+    format.html { render :contact, locals: { feedback: {} } }
+  end
+ end
+ 
+ def leave_feedback
+  required = [:name, :email, :reply, :feedback_type, :message]
+  form_complete = true
+  required.each do |f|
+    if params.has_key? f and not params[f].blank?
+      # that's good news. do nothing
+    else
+      form_complete = false
+    end
+  end
+  if form_complete
+    form_status_msg = 'Thank you for your feedback!'
+  else
+    form_status_msg = 'Please fill in all the remaining form fields and resubmit.'
+  end 
+  respond_to do |format|
+    format.html { render :contact, locals: { status_msg: form_status_msg, feedback: params } }
+  end
+ end
+ 
 end
